fe663726180ae25ed6c54556683dc9563013d838
Jeisy Liu <36287124+JeisyLiu@users.noreply.github.com>
2020-08-28 19:25:38 +0800

    ToLuaConnectedScript
    Tolua tracked for Unity programming// 
    Notice:
    Modify the module in the project with extra library

---
+++ toluaGlobal/toLuaKeywords.lua
@@ -0,0 +1,62 @@
+
+Basic:
+Vector3     Vector2
+Material
+UnityEngine
+
+------------------------------------------------------------------------------------------------------          Transform
+Transform   transform
+
+GetComponent    
+GetComponentInChildren  
+GetComponentInParent
+GetChild    
+GetChildCount   
+GetSiblingIndex
+GetInstanceID
+SetParent
+parent
+root
+
+Translate
+position
+localPosition
+
+Rotate
+RotateAround
+rotation
+
+localScale
+
+-----------------------------------------------------------------------------------------------------          GameObject
+
+GameObject  gameObject
+
+Instantiate 
+Destroy
+FindGameObjectsWithTag
+SetActive
+
+------------------------------------------------------------------------------------------------------          RectTransformUtility
+
+RectTransformUtility
+
+ScreenPointToWorldPointInRectangle
+
+------------------------------------------------------------------------------------------------------          Animator
+
+Animator    Animation
+
+GetCurrentAnimatorStateInfo
+
+------------------------------------------------------------------------------------------------------          Camera
+
+Camera
+
+ScreenPointToRay
+
+-----------------------------------------------------------------------------------------------------          Timer
+
+WaitForSeconds
+
+-----------------------------------------------------------------------------------------------------          Physic
